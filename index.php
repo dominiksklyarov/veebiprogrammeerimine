14ebbf034224390977ac02a22e3fd8149c17fcb0
Dominik Sklyarov <donminiksklyarov@users.noreply.github.com>
2018-09-10 10:40:55 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<title>Dominik Sklyarov</title>
+
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+</head>
+<body>
+
+<div class="container">
+<h1>Informaatika</h1>
+
+	<p> No idea what I need this text for </p>
+
+	<a href="http://dominiksklyarov.com" style="color:red"><p>Welcome to my site</p></a>
+
+	<a href="https://www.instagram.com/dominiksklyarov" style="color:red"><p>Follow me on instagram</p></a>
+
+	<a href="https://www.facebook.com/dominiksklyarov" style="color:red"><p>Add me on facebook</p></a>
+	<p> or you can add me on <a href="https://www.snapchat.com/add/dominiksklyarov" style="color:#FFCC33"> snapchat </a> </p>
+
+<a href="http://www.dominiksklyarov.com"><img src="http://dominiksklyarov.com/wp-content/uploads/2018/08/IMG_4781.jpg" alt="dominiksklyarov.us" width="50%" height="50%"></a> <!-- touch this and I'll find you. To use this picture, contact me. -->
+
+</div>
+</body>
+
+</html>
