2239ba0b8c89089cc7c23cb63114157457041aa4
Dominik Sklyarov <donminiksklyarov@users.noreply.github.com>
2018-09-10 11:36:05 +0300

    tegin natuke asju juurde

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
+<?php
+	echo "Siin on minu esimene PHP";
+	$name = "Dominik";
+	$surname = "Sklyarov";
+	$todayDate = date("d.m.y");
+	$hourNow = date("H");
+	$partOfDay ="";
+	if ($hourNow < 8) {
+		$partOfDay = "Varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "kooliaeg";
+	}
+	if ($hourNow >= 16) {
+		$partOfDay = "vabaaeg";
+	}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
 
 	<meta charset="utf-8">
-	<title>Dominik Sklyarov</title>
+	<title>Dominik Sklyarov
+	</title>
 
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
 
@@ -12,15 +33,25 @@
 <body>
 
 <div class="container">
-<h1>Informaatika</h1>
+	<h1>
+		<?php 
+
+		echo $name ." " .$surname;
+
+		?></h1>
 
 	<p> No idea what I need this text for </p>
+	<?php
+	echo "<p>Tänane kuupäev on: " .$todayDate ."</p> \n";
+	
+	echo "<p>Hetkel on kell " .date("H:i:s") .", käes on " .$partOfDay .".</p> \n";
+	?>
 
-	<a href="http://dominiksklyarov.com" style="color:red"><p>Welcome to my site</p></a>
+	<p><a href="http://dominiksklyarov.com" style="color:red">Welcome to my site</a></p>
 
-	<a href="https://www.instagram.com/dominiksklyarov" style="color:red"><p>Follow me on instagram</p></a>
+	<p><a href="https://www.instagram.com/dominiksklyarov" style="color:red">Follow me on instagram</a></p>
 
-	<a href="https://www.facebook.com/dominiksklyarov" style="color:red"><p>Add me on facebook</p></a>
+	<p><a href="https://www.facebook.com/dominiksklyarov" style="color:red">Add me on facebook</a></p>
 	<p> or you can add me on <a href="https://www.snapchat.com/add/dominiksklyarov" style="color:#FFCC33"> snapchat </a> </p>
 
 <a href="http://www.dominiksklyarov.com"><img src="http://dominiksklyarov.com/wp-content/uploads/2018/08/IMG_4781.jpg" alt="dominiksklyarov.us" width="50%" height="50%"></a> <!-- touch this and I'll find you. To use this picture, contact me. -->
