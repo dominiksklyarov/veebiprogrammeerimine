be382d3c402be22038cfe1c6548bc5d453d8961c
Dominik Sklyarov <donminiksklyarov@users.noreply.github.com>
2018-09-17 11:37:47 +0300

    lisatud stuffi

--- Tund 3/photo.php
+++ Tund 3/photo.php
@@ -2,17 +2,29 @@
 	echo "Sa sattusid .php veebikale!";
 	$name = "Dominik";
 	$surname = "Sklyarov";
-	$dirToRead = "../../../rinde/public_html/veebiprogrammeerimine2018s/pics";
-	$allFiles = array_slice(scandir($dirToRead), 2);
-	var_dump($_POST);
-
-	if (isset($_POST["fName"])) {
-		$name = $_POST["fName"];
+	$todayDate = date("d.m.y");
+	$hourNow = date("H");
+	$partOfDay ="";
+	if ($hourNow < 8) {
+		$partOfDay = "Varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "kooliaeg";
+	}
+	if ($hourNow >= 16) {
+		$partOfDay = "vabaaeg";
 	}
 
-	if (isset($_POST["sName"])) {
-		$surname = $_POST["sName"];
-	}
+	$weekDayNow = date("N");
+	$weekDays = ["Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"];
+	$weekDay = $weekDays[date("N")-1];
+//loosime juhusliku pildi
+$picNum = mt_rand(2, 43);
+$picURL = "http://www.cs.tlu.ee/~rinde/media/fotod/TLU_600x400/tlu_";
+$picEXT = ".jpg";
+$picFileName = $picURL .$picNum .$picEXT;
+
+
 ?>
 
 <!DOCTYPE html>
@@ -30,19 +42,19 @@
 <body>
 
 <div class="container">
-	
 	<h1>
-		
 		<?php 
 
 		echo $name ." " .$surname;
 
-		?>
-		
-	</h1>
+		?></h1>
 
 	<p> No idea what I need this text for </p>
+	<?php
+	echo "<p>Täna on: " .$weekDay ." " .$todayDate ." </p> \n";
 	
+	echo "<p>Hetkel on kell " .date("H:i:s") .", käes on " .$partOfDay .".</p> \n";
+	?>
 
 	<p><a href="http://dominiksklyarov.com" style="color:red">Welcome to my site</a></p>
 
@@ -53,36 +65,10 @@
 
 <a href="http://www.dominiksklyarov.com"><img src="http://dominiksklyarov.com/wp-content/uploads/2018/08/IMG_4781.jpg" alt="dominiksklyarov.us" width="50%" height="50%"></a> <!-- touch this and I'll find you. To use this picture, contact me. -->
 
-<?php  
+<img src="<?php echo $picFileName; ?>" width="50%" height="50%" alt="do not even touch this picture">
 
-echo '<img src = " ' .$dirToRead .$allFiles[1] .'" alt="picture"><br>';
 
-?>
 
-<hr>
-
-<form method="POST">
-	<label>First name:</label>
-	<input type="text" name="fName" value=""><br>
-	<label>Second name:</label>
-	<input type="text" name="sName"><br>
-	<label>Birth year:</label>
-	<input type="number" name="byear" min="1924" max="2003" value="1998"><br>
-	<input type="submit" value="SUBMIT INFORMATION" name="submitdata">
-</form>
-
-<?php  
-
-	if (isset($_POST["submitdata"])) {
-		echo "<p> Olete elanud järgmistel aastatel: </p>";
-		echo "<ul> \n";
-		for ($i = $_POST["byear"]; $i <= date("Y"); $i ++) {
-			echo "<li>" . $i ." </li> \n";
-		}
-
-	}
-
-?>
 </div>
 </body>
 
